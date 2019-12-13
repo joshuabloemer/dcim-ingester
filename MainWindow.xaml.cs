6dd654de70c3f3b735c5ad73009f27838321c52d
Henry Hunt <henryhunt.dev@gmail.com>
2019-12-13 19:27:13 +0000

    Added auto-detection of new volumes

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,24 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Management;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Interop;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using static dcim_ingester.VolumeWatcher;
 
 namespace dcim_ingester
 {
     public partial class MainWindow : Window
     {
+        private List<IngesterTask> Tasks = new List<IngesterTask>();
+        private List<string> Volumes = new List<string>();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -29,6 +26,130 @@ namespace dcim_ingester
             Rect workingArea = SystemParameters.WorkArea;
             Left = workingArea.Right - Width - 20;
             Top = workingArea.Bottom - Height - 20;
+
+            Volumes = GetVolumes();
+            PrintVolumes(Volumes);
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+
+            // Register for device change messages
+            HwndSource source = HwndSource.FromHwnd(new WindowInteropHelper(this).Handle);
+            if (source != null)
+            {
+                source.AddHook(WindowMessageHandler);
+                VolumeWatcher.RegisterDeviceNotification(source.Handle);
+            }
+        }
+        private IntPtr WindowMessageHandler(
+            IntPtr hwnd, int msg, IntPtr wparam, IntPtr lparam, ref bool handled)
+        {
+            if (msg == VolumeWatcher.WM_DEVICE_CHANGE)
+            {
+                switch ((int)wparam)
+                {
+                    case VolumeWatcher.DBT_DEVICE_ARRIVAL:
+                        {
+                            DevBroadcastDeviceInterface ver = (DevBroadcastDeviceInterface)
+                                Marshal.PtrToStructure(lparam, typeof(DevBroadcastDeviceInterface));
+                            if (!ver.Name.StartsWith("\\\\?\\")) break;
+
+                            new Thread(delegate () { VolumeMounted(); }).Start();
+                            break;
+                        }
+
+                    case VolumeWatcher.DBT_DEVICE_REMOVE_COMPLETE:
+                        {
+                            DevBroadcastDeviceInterface ver = (DevBroadcastDeviceInterface)
+                                Marshal.PtrToStructure(lparam, typeof(DevBroadcastDeviceInterface));
+                            if (!ver.Name.StartsWith("\\\\?\\")) break;
+
+                            new Thread(delegate () { VolumeUnmounted(); }).Start();
+                            break;
+                        }
+                }
+            }
+
+            handled = false;
+            return IntPtr.Zero;
+        }
+
+
+        private void VolumeMounted()
+        {
+            Application.Current.Dispatcher.Invoke(delegate ()
+            {
+                List<string> newVolumes = GetVolumes();
+                foreach (string volume in newVolumes)
+                {
+                    if (!Volumes.Contains(volume))
+                    {
+                        string driveLetter = GetDriveLetter(volume);
+                        Console.WriteLine("new -- " + driveLetter);
+
+                        if (Directory.Exists(Path.Combine(driveLetter, "DCIM")))
+                            StartNewTask(driveLetter);
+                    }
+                }
+
+                Volumes = GetVolumes();
+            });
+        }
+        private void VolumeUnmounted()
+        {
+            Application.Current.Dispatcher.Invoke(delegate ()
+            {
+                Volumes = GetVolumes();
+            });
+        }
+
+        private void StartNewTask(string driveLetter)
+        {
+            IngesterTask task = new IngesterTask(driveLetter);
+            task.Margin = new Thickness(0, 20, 0, 0);
+            Tasks.Add(task);
+            StackPanelTasks.Children.Add(task);
+
+            Height += 140;
+            Top -= 140;
+        }
+
+        private List<string> GetVolumes()
+        {
+            List<string> volumes = new List<string>();
+            ManagementObjectSearcher query = new ManagementObjectSearcher(
+                "SELECT DeviceID FROM Win32_Volume WHERE DriveLetter != NULL");
+
+            foreach (ManagementObject volume in query.Get())
+            {
+                // Get GUID only to avoid a mess with slashes and escaping
+                string deviceId = volume["DeviceID"].ToString();
+                deviceId = deviceId.Substring(deviceId.IndexOf('{'),
+                    deviceId.IndexOf('}') - deviceId.IndexOf('{') + 1);
+                volumes.Add(deviceId);
+            }
+
+            return volumes;
+        }
+        private string GetDriveLetter(string deviceId)
+        {
+            ManagementObjectSearcher query = new ManagementObjectSearcher(string.Format(
+                "SELECT DriveLetter FROM Win32_Volume WHERE DeviceID LIKE '%{0}%'", deviceId));
+            ManagementObjectCollection result = query.Get();
+
+            if (result.Count > 0)
+                return result.OfType<ManagementObject>().First()["DriveLetter"].ToString();
+            return null;
+        }
+
+        private void PrintVolumes(List<string> volumes)
+        {
+            Console.WriteLine("VOLUMES:");
+
+            foreach (string s in volumes)
+                Console.WriteLine(GetDriveLetter(s) + " -- " + s);
         }
     }
 }
---
+++ VolumeWatcher.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace dcim_ingester
+{
+    // Taken from https://stackoverflow.com/questions/1976573/using-registerdevicenotification-in-a-net-app
+    public static class VolumeWatcher
+    {
+        public const int WM_DEVICE_CHANGE = 0x0219; // Event for some device changed
+        public const int DBT_DEVICE_ARRIVAL = 0x8000; // Event for device arrival
+        public const int DBT_DEVICE_REMOVE_COMPLETE = 0x8004; // Event for device removal
+
+        private const int DBT_DEVICE_TYPE_DEVICE_INTERFACE = 5;
+
+        private static readonly Guid GUID_DEVICE_INTERFACE_VOLUME
+            = new Guid("53F5630D-B6BF-11D0-94F2-00A0C91EFB8B"); // Volume device group
+
+        private static IntPtr NotificationHandle;
+
+
+        public static void RegisterDeviceNotification(IntPtr windowHandle)
+        {
+            DevBroadcastDeviceInterface dbi = new DevBroadcastDeviceInterface
+            {
+                DeviceType = DBT_DEVICE_TYPE_DEVICE_INTERFACE,
+                Reserved = 0,
+                ClassGuid = GUID_DEVICE_INTERFACE_VOLUME,
+                Name = ""
+            };
+
+            dbi.Size = Marshal.SizeOf(dbi);
+            IntPtr buffer = Marshal.AllocHGlobal(dbi.Size);
+            Marshal.StructureToPtr(dbi, buffer, true);
+
+            NotificationHandle = RegisterDeviceNotification(windowHandle, buffer, 0);
+        }
+        public static void UnregisterDeviceNotification()
+        {
+            UnregisterDeviceNotification(NotificationHandle);
+        }
+
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr RegisterDeviceNotification(
+            IntPtr recipient, IntPtr notificationFilter, int flags);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterDeviceNotification(IntPtr handle);
+
+        // Taken from https://stackoverflow.com/questions/2208722/how-to-get-friendly-device-name-from-dev-broadcast-deviceinterface-and-device-ins
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        public struct DevBroadcastDeviceInterface
+        {
+            public int Size;
+            public int DeviceType;
+            public int Reserved;
+            public Guid ClassGuid;
+
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 255)]
+            public string Name;
+        }
+    }
+}
