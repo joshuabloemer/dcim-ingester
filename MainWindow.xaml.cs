5a4358952627bb9223a174ed8596133d15f9e827
Henry Hunt <henryhunt.dev@gmail.com>
2019-12-13 13:48:57 +0000

    Initial window design

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -8,6 +9,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -15,14 +17,18 @@ using System.Windows.Shapes;
 
 namespace dcim_ingester
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Rect workingArea = SystemParameters.WorkArea;
+            Left = workingArea.Right - Width - 20;
+            Top = workingArea.Bottom - Height - 20;
         }
     }
 }
