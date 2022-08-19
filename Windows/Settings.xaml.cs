71e161730510415b807446f61a1dc93d464f5fb5
joshuabloemer <joshuabloemer@gmail.com>
2022-08-19 05:23:12 -0700

    Deleted all code related to Rules UI
    UI development will happen in a separate Branch

--- Windows/Settings.xaml.cs
+++ Windows/Settings.xaml.cs
@@ -17,10 +17,6 @@ namespace DcimIngester.Windows
         {
             TextBoxDestination.Text = Properties.Settings.Default.Destination;
             ComboBoxSubfolders.SelectedIndex = Properties.Settings.Default.Subfolders;
-            // Initialize first Rule
-            Rule Rule0 = new Rule(0);
-            // Generate_Ui(Rule0);
-
         }
 
         private void TextBoxDestination_TextChanged(object sender, TextChangedEventArgs e)
@@ -66,49 +62,5 @@ namespace DcimIngester.Windows
         {
             Close();
         }
-
-        private void Generate_Ui(Rule initial_rule)
-        {
-            Console.WriteLine("generating");
-
-        }
-
-        // private void Rule1_TextChanged(object sender, TextChangedEventArgs e)
-        // {
-        //     TextBox textBox = sender as TextBox ?? throw new Exception("TextChanged was called without a TextBox, this should not happen");
-        //     Console.WriteLine(textBox.Text);
-        //     Console.WriteLine(textBox.Text.Length);
-        //     Console.WriteLine(textBox.Text.GetType());
-        //     if (textBox.Text.Length > 0) 
-        //     {
-        //         // make sure all empty fields are present
-        //         // create if not
-        //         string indentlevel = textBox.Name.Substring(4);
-        //         string[] levels = indentlevel.Split("_");
-        //         // Console.WriteLine(indentlevel);
-        //         // // Console.WriteLine(levels[0]);
-        //         // // Console.WriteLine(levels[1]);
-        //         // var under = this.FindName("Rule" + String.Join("_",levels[0..^2]) +"_"+ levels[^2] +"_"+ (Int32.Parse(levels[^1])+1));
-        //         // var indent = this.FindName(textBox.Name+"1_1");
-        //         // var next = this.FindName("Rule" + (Int32.Parse(levels[^1])+1) +"_"+ 1);
-        //         // //levels[^2] +"_"+ (Int32.Parse(levels[^1])+1))
-
-        //         // Console.WriteLine("Rule" + String.Join("_",levels[0..^1]) +"_"+ (Int32.Parse(levels[^1])+1));
-        //         // Console.WriteLine(textBox.Name+"_1_1");
-        //         // // TODO: fix issue where extra underscore is placed before  
-        //         // Console.WriteLine("Rule" + String.Join("_",new string[]{String.Join("_",levels[0..^2]),(Int32.Parse(levels[^2])+1) +"_1"}.Where(s => !string.IsNullOrEmpty(s))));
-
-
-        //         // // create textbox
-        //         // if (under == null)
-        //         // {
-        //         //     Console.WriteLine("null");
-        //         // }
-        //         // Console.WriteLine(under);
-
-        //         // Console.WriteLine(textBox.Name.Substring(4));
-
-        //     }
-        // }
     }
 }
