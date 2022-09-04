77202a5ae4b3079d8029166475f188854a6fc047
joshuabloemer <joshuabloemer@gmail.com>
2022-09-04 14:41:32 +0200

    Added function GetDateTaken
    Returns the creation time of the provided filepath

--- Utilities.cs
+++ Utilities.cs
@@ -120,5 +120,31 @@ namespace DcimIngester
             newPath = destination;
             renamed = duplicates != 0;
         }
+
+        /// <summary>
+        /// Gets the creation time of a file from embeded Metadata if possible, falls back to the system specific properties if not.  
+        /// </summary>
+        /// <param name="path">The path to the file.</param>
+        /// <returns>The creation time as <see langword="DateTime"/> object.</returns>
+        public static DateTime GetDateTaken(string path)
+        {
+            // Try to read Creation Time from metadata (only works for supported image formats)
+            try{
+                IEnumerable<MetadataExtractor.Directory> metadata = MetadataExtractor.ImageMetadataReader.ReadMetadata(path);
+                // Search all exif subdirs for TagDateTimeOriginal 
+                foreach (ExifSubIfdDirectory exif in metadata.OfType<ExifSubIfdDirectory>()) 
+                {
+                    string? dto = exif?.GetDescription(ExifDirectoryBase.TagDateTimeOriginal);
+                    if (dto != null) 
+                        return DateTime.ParseExact(dto, "yyyy:MM:dd HH:mm:ss", null);
+                }
+            }
+            catch (Exception ex) when (ex is MetadataExtractor.ImageProcessingException || ex is FormatException){}                            
+            
+            // Fallback to Creation Time and Modification Time for all other file types
+            DateTime modifiedTime = File.GetLastWriteTime(path);
+            DateTime creationTime = File.GetCreationTime(path);
+            return creationTime <= modifiedTime ? creationTime:modifiedTime;
+        }
     }
 }
