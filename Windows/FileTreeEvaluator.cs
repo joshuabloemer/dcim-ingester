c6d775feffbf2bc18b7351da08b8edbf404d45a1
joshuabloemer <joshuabloemer@gmail.com>
2022-09-20 13:05:26 +0200

    Added review file tree in settings window

---
+++ Windows/FileTreeEvaluator.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using MetadataExtractor;
+using System.IO;
+using static DcimIngester.Utilities;
+
+namespace DcimIngester.Windows {
+    class FileTreeEvaluator {
+        
+        public FileTreeEvaluator(){}
+
+        public object Evaluate(SyntaxNode node) {
+            switch(node) {
+                case BlockNode b: return blockNode(b);
+                case RuleNode r: return ruleNode(r);
+                case PathNode p: return PathToString(p);
+                case StringNode s: return s.Value;
+                case ExtensionNode: return "extension";
+                case YearNode: return "YYYY";
+                case MonthNode: return "MM";
+                case DayNode: return "DD";
+                case HourNode: return "hh";
+                case MinuteNode: return "mm";
+                case SecondNode: return "ss";
+                case FileNameNode: return "file name";
+                case PathPartNode p: return $"path[{p.Part}]";
+                case PathNameNode: return "path";
+                case MetadataNode m: return $"{m.Directory};{m.Tag}";
+            }
+            throw(new Exception($"Unknown node type {node.GetType()}"));
+        }
+
+        private string PathToString(PathNode path){
+            string result = "";
+            foreach (SyntaxNode part in path.Parts){
+                result = Path.Join(result,Convert.ToString(Evaluate(part)));
+            }
+            return result;
+        }
+
+        private List<string> ruleNode(RuleNode r)
+        {
+            List<String> result = new List<string>();
+
+            if (r.GetIndent() is not null){
+                foreach(string path in (List<String>)Evaluate(r.GetIndent())){
+                    if (r.Path is not EmptyNode)
+                        result.Add(Path.Join((string)Evaluate((PathNode)r.Path),path));
+                    else
+                        result.Add(path);
+                }
+            }
+            else{
+                result.Add((string)Evaluate((PathNode)r.Path));
+            }
+            if (r.Under is not EmptyNode){
+                result.AddRange((List<String>)Evaluate(r.Under));
+            }
+
+            return result;
+        }
+
+        private List<String> blockNode(BlockNode b)
+        {
+            List<String> result = new List<string>();
+            foreach (RuleNode rule in b.Statements){
+                result.AddRange((List<String>)Evaluate(rule));
+            }
+            return result;
+
+        }
+    }
+}
--- Windows/Settings.xaml.cs
+++ Windows/Settings.xaml.cs
@@ -4,6 +4,9 @@ using FolderBrowserDialog = System.Windows.Forms.FolderBrowserDialog;
 using OpenFileDialog = System.Windows.Forms.OpenFileDialog;
 using System;
 using System.Linq;
+using DcimIngester.Rules;
+using System.IO;
+using System.Collections.Generic;
 
 namespace DcimIngester.Windows
 {
@@ -59,19 +62,93 @@ namespace DcimIngester.Windows
             else ButtonSave.IsEnabled = false;
         }
 
+
+        private void ButtonReloadFileTree_Click(object sender, RoutedEventArgs e)
+        {
+            if (Properties.Settings.Default.Rules != ""){
+                var parser = new Parser();
+                var tree = parser.Parse(File.ReadAllText(Properties.Settings.Default.Rules));
+                var evaluator = new FileTreeEvaluator();
+                List<String> paths = (List<String>)evaluator.Evaluate(tree.Block);
+
+                PopulateTreeView(FileTreeView, paths, '/');
+            }
+        }
+
+        private void PopulateTreeView(TreeView treeView, IEnumerable<string> paths, char pathSeparator) {
+            List<MyTreeViewItem> sourceCollection = new List<MyTreeViewItem>();
+            foreach (string path in paths) {
+                string[] fileItems = path.Split(pathSeparator, StringSplitOptions.RemoveEmptyEntries);
+                if (fileItems.Any()) {
+
+                MyTreeViewItem root = sourceCollection.FirstOrDefault(x=>x.Name.Equals(fileItems[0]) && x.Level.Equals(1));
+                if (root == null) {
+                    root = new MyTreeViewItem()
+                    {
+                    Level = 1,
+                    Name = fileItems[0],
+                    SubItems = new List<MyTreeViewItem>()
+                    };
+                    sourceCollection.Add(root);
+                }
+
+                if (fileItems.Length > 1) {
+
+                    MyTreeViewItem parentItem = root;
+                    int level = 2;
+                    for (int i = 1; i < fileItems.Length; ++i) {
+
+                    MyTreeViewItem subItem = parentItem.SubItems.FirstOrDefault(x => x.Name.Equals(fileItems[i]) && x.Level.Equals(level));
+                    if (subItem == null) {
+                        subItem = new MyTreeViewItem()
+                        {
+                        Name = fileItems[i],
+                        Level = level,
+                        SubItems = new List<MyTreeViewItem>()
+                        };
+                        parentItem.SubItems.Add(subItem);
+                    }
+
+                    parentItem = subItem;
+                    level++;
+                    }
+                }
+                }
+            }
+
+            treeView.ItemsSource = sourceCollection;
+            }
+
         private void ButtonSave_Click(object sender, RoutedEventArgs e)
         {
             Properties.Settings.Default.Destination = TextBoxDestination.Text;
             Properties.Settings.Default.Rules = TextBoxRules.Text;
 
             Properties.Settings.Default.Save();
-
             Close();
         }
 
         private void ButtonCancel_Click(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+    }
+    
+    public class MyTreeViewItem
+    {
+        public int Level {
+            get;
+            set;
+        }
+
+        public string Name {
+            get;
+            set;
+        }
+
+        public List<MyTreeViewItem> SubItems {
+            get;
+            set;
         }
     }
 }
